3842092ecda1e6a3580106c02098a570aeaebd06
Julia Zorina <yulia.a.zzo@gmail.com>
2024-10-28 16:08:46 +0400

    Добавлены миграции

---
+++ HabitTracker.Data/HabitTrackerContext.cs
@@ -0,0 +1,52 @@
+﻿using HabitTracker.Core;
+using HabitTracker.Core.Entities;
+using HabitTracker.Data.Configurations;
+using HabitTracker.Data.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
+namespace HabitTracker.Data
+{
+  public class HabitTrackerContext : DbContext
+  {
+    private readonly StreamWriter logStream = new StreamWriter("mylog.txt", true);
+
+    public DbSet<UserEntity> Users { get; set; }
+    public DbSet<HabitEntity> Habits { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.ApplyConfiguration(new UserConfiguration());
+      modelBuilder.ApplyConfiguration(new HabitConfiguration());
+
+      base.OnModelCreating(modelBuilder);
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseNpgsql(ApplicationData.ConfigApp.DatabaseConnectionString);
+      //optionsBuilder.LogTo(Console.WriteLine, new[] { RelationalEventId.CommandExecuted });
+      optionsBuilder.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name });
+      optionsBuilder.LogTo(logStream.WriteLine, Microsoft.Extensions.Logging.LogLevel.Error);
+    }
+
+    public override async ValueTask DisposeAsync()
+    {
+      await base.DisposeAsync();
+      await logStream.DisposeAsync();
+    }
+    
+    #region Конструторы
+
+    public HabitTrackerContext(DbContextOptions<HabitTrackerContext> options) 
+      :base(options)
+    {
+      //Database.EnsureDeleted(); // гарантируем, что бд удалена
+      //Database.EnsureCreated();   // гарантируем, что БД создана
+    }
+
+
+    #endregion
+  }
+}
---
+++ HabitTracker.Data/Migrations/20241028115227_InitialCreate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HabitTracker.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    ChatId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Habits",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    ProgressDays = table.Column<long>(type: "bigint", nullable: false),
+                    CreationDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    LastExecutionDate = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Habits", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Habits_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Habits_UserId",
+                table: "Habits",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Habits");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
