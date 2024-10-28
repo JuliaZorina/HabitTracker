7eff9d85a4e2fe8ae0a46778284ad5ae2296b9c4
Julia Zorina <yulia.a.zzo@gmail.com>
2024-10-28 19:06:17 +0400

    Добавлены CRUD операции для добавления пользователей в бд

---
+++ HabitTracker.Data/Repositories/UsersRepository.cs
@@ -0,0 +1,82 @@
+﻿using HabitTracker.Core;
+using HabitTracker.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HabitTracker.Data.Repositories
+{
+  public class UsersRepository
+  {
+    #region Поля и свойства
+
+    private readonly HabitTrackerContext _dbContext;
+
+    #endregion
+
+    #region Методы
+
+    /// <summary>
+    /// Получить список всех пользователей.
+    /// </summary>
+    /// <returns>Асинхронно возвращает коллекцию всех привычек в базе данных.</returns>
+    public async Task<List<UserEntity>> Get()
+    {
+      return await _dbContext.Users
+        .AsNoTracking()
+        .ToListAsync();
+    }
+
+    /// <summary>
+    /// Получить пользователя по его уникальному идентификатору.
+    /// </summary>
+    /// <returns>Асинхронно возвращает привычку, найденную по ее уникальному идентификатору.</returns>
+    public async Task<HabitEntity?> GetById(Guid id)
+    {
+      return await _dbContext.Habits
+        .AsNoTracking()
+        .FirstOrDefaultAsync(u => id == u.Id);
+    }
+
+
+    /// <summary>
+    /// Получить пользователя по идентификатору его чата в Telegram.
+    /// </summary>
+    /// <returns>Асинхронно возвращает привычку, найденную по ее уникальному идентификатору.</returns>
+    public async Task<UserEntity?> GetByChatId(long id)
+    {
+      return await _dbContext.Users
+        .AsNoTracking()
+        .FirstOrDefaultAsync(u => id == u.ChatId);
+    }
+
+    /// <summary>
+    /// Добавить нового пользователя в базу данных.
+    /// </summary>
+    /// <param name="id">Уникальный идентификатор пользоваетля.</param>
+    /// <param name="name">Имя пользователя.</param>
+    /// <param name="chatId">Уникальный идентификатор чата пользователя в Telegram.</param>
+    /// <returns></returns>
+    public async Task Add(Guid id, string name, long chatId)
+    {      
+      var habit = new UserEntity
+      {
+        Id = id,
+        Name = name,
+        ChatId = chatId
+      };
+
+      await _dbContext.AddAsync(habit);
+      await _dbContext.SaveChangesAsync();
+    }
+
+    #endregion
+
+    #region Конструкторы
+
+    public UsersRepository(HabitTrackerContext dbContext)
+    {
+      _dbContext = dbContext;
+    }
+
+    #endregion
+  }
+}
